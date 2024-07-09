1e539be35e3ef515138c59a73b137b16d0dd2d1d
badAl <badalsingh120403@gmail.com>
2024-07-09 13:33:54 +0530

    feat: added updateTodo functionality

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -15,6 +15,7 @@ function AddTodo() {
 
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      {/* <h1 className="text-3xl bg-slate-200 rounded-lg shadow-md text-center m-4 p-2 "> */}
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
